61b6e81b46dd851b77c44c945378e73b28e1db42
Abid <abidroid1@gmail.com>
2025-04-30 11:25:26 +0500

    opr

--- bin/uob_day1_dart.dart
+++ bin/uob_day1_dart.dart
@@ -1,57 +1,20 @@
+import 'dart:io';
+
 void main() {
+  // String name;
+  // stdout.write( 'Enter your name : ');
+  // name = stdin.readLineSync()!;
+  //
+  // print('Welcome $name');
 
-  print('welcome to Dart');
+  int n1, n2;
+  int result;
+  stdout.write("Enter first number : ");
+  n1 = int.parse(stdin.readLineSync()!);
 
-  int totalStudents;
-  totalStudents = 30;
+  stdout.write('Enter second number: ');
+  n2 = int.parse(stdin.readLineSync()!);
 
-  print(totalStudents);
-
-  double gpa = 3.3;
-  print(gpa);
-
-  String name = "Muhammad Ali Jinnah";
-  print(name);
-
-
-  final double pi = 3.14;
-  print(pi);
-
-
-  // type inference
-  var address = "Bagh AJK";
-  print(address.runtimeType);
-
-
-  dynamic variable = 3.3;
-  print(variable);
-  variable = "Ali";
-  print(variable);
-  variable = true;
-  print(variable);
-  variable = 55;
-  print(variable);
-
-  // Optional
-
-  String? cnic;
-  print(cnic);
-
-  String? fianceName;
-  print(fianceName);
-
-  String firstName = 'Muhammad';
-  String lastName = "Abid";
-
-  String fullName = firstName + " " + lastName;
-
-  // String interpolation
-  String fName = "$firstName $lastName";
-  print(fName);
-
-  int age = 50;
-  print("My age is $age");
-
-  print('After 1 year i will be ${age+1} years old');
-
+  result = n1 % n2;
+  print('Remainder = $result');
 }
