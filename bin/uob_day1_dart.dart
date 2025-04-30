4c8ec31376b3e94a7ff1611d1fec000dca2e7891
Abid <abidroid1@gmail.com>
2025-04-30 15:41:02 +0500

    function call within function call

--- bin/uob_day1_dart.dart
+++ bin/uob_day1_dart.dart
@@ -10,7 +10,35 @@ void displayMyString( String str ){
 }
 
 void sum( int x, int y ) => print('Sum = ${x+y}');
-void square( int value ) => print( 'Square of $value is ${value*value}');
+
+int square( int radius )
+{
+  return radius*radius;
+}
+
+// Boss
+  void main() {
+
+  print( square( square(2) ) );
+
+
+    //table(range: 9, number: 8);
+
+    // sum(5, 6);
+    // square(7);
+    // sum(2, 3);
+    // sum(59, 1);
+
+    // displayMyString("#"); // actual arguments
+    // displayMyString("+");
+    // displayStars();
+  }
+
+  void displayStars()
+  {
+    print('**************************');
+  }
+
 
 // Positional parameters
 // Named Parameters
@@ -19,23 +47,4 @@ void table( {required int number, required int range} )
   for( var i = 1; i <= range; i++ ){
     print('$number X $i = ${number * i}');
   }
-}
-
-// Boss
-void main() {
-  table(range: 9, number: 8);
-
-  // sum(5, 6);
-  // square(7);
-  // sum(2, 3);
-  // sum(59, 1);
-
-  // displayMyString("#"); // actual arguments
-  // displayMyString("+");
-  // displayStars();
-}
-
-void displayStars()
-{
-  print('**************************');
 }
