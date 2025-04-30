3a4f6abdd5c07fa2e681a69d87a4a6c52d28b438
Abid <abidroid1@gmail.com>
2025-04-30 11:58:27 +0500

    simple calcu

--- bin/uob_day1_dart.dart
+++ bin/uob_day1_dart.dart
@@ -2,18 +2,38 @@ import 'dart:io';
 
 void main() {
 
-  int n;
-  stdout.write("Enter a number less than ten : ");
-  n = int.parse(stdin.readLineSync()!);
+  int n1, n2, choice;
+  stdout.write("Enter first number : ");
+  n1 = int.parse(stdin.readLineSync()!);
 
-  if( n < 10 )
-  {
-    print("Shaabaash");
+  stdout.write("Enter second number : ");
+  n2 = int.parse(stdin.readLineSync()!);
+
+  String message = '''
+Select Operation
+1. Add
+2. Sub
+3. Mul
+4. Div
+Your choice ? ''';
+
+  stdout.write(message);
+  choice = int.parse(stdin.readLineSync()!);
+
+  if( choice == 1){
+    print('Sum = ${n1+n2}');
+  }
+  else if( choice == 2){
+    print('Difference = ${n1-n2}');
+  }
+  else if( choice == 3){
+    print("Product = ${n1*n2}");
+  }
+  else if( choice == 4){
+    print('Quotient = ${n1/n2}');
   }
   else
   {
-      print("Very Bad");
+    print('Invalid choice');
   }
-
-  print('End');
 }
