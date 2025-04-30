e4461f699ccf889d3f967d8b726954cecb28350c
Abid <abidroid1@gmail.com>
2025-04-30 12:02:26 +0500

    ? :

--- bin/uob_day1_dart.dart
+++ bin/uob_day1_dart.dart
@@ -2,38 +2,20 @@ import 'dart:io';
 
 void main() {
 
-  int n1, n2, choice;
+  int n1, n2, large;
   stdout.write("Enter first number : ");
   n1 = int.parse(stdin.readLineSync()!);
 
   stdout.write("Enter second number : ");
   n2 = int.parse(stdin.readLineSync()!);
 
-  String message = '''
-Select Operation
-1. Add
-2. Sub
-3. Mul
-4. Div
-Your choice ? ''';
+  large = (n1 > n2) ? n1 : n2;
 
-  stdout.write(message);
-  choice = int.parse(stdin.readLineSync()!);
+  // if( n1 > n2 ){
+  //   large = n1;
+  // }else {
+  //   large = n2;
+  // }
 
-  if( choice == 1){
-    print('Sum = ${n1+n2}');
-  }
-  else if( choice == 2){
-    print('Difference = ${n1-n2}');
-  }
-  else if( choice == 3){
-    print("Product = ${n1*n2}");
-  }
-  else if( choice == 4){
-    print('Quotient = ${n1/n2}');
-  }
-  else
-  {
-    print('Invalid choice');
-  }
+  print('Large number is $large');
 }
