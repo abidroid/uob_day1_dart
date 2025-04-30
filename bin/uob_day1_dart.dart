b5a462a9ec0525806890d87c97040f9d345dc459
Abid <abidroid1@gmail.com>
2025-04-30 15:30:02 +0500

    functions

--- bin/uob_day1_dart.dart
+++ bin/uob_day1_dart.dart
@@ -1,26 +1,41 @@
 import 'dart:io';
 
+
+void displayMyString( String str ){
+
+  for( var i = 1; i < 31; i++ ){
+    stdout.write(str);
+  }
+  print('');
+}
+
+void sum( int x, int y ) => print('Sum = ${x+y}');
+void square( int value ) => print( 'Square of $value is ${value*value}');
+
+// Positional parameters
+// Named Parameters
+void table( {required int number, required int range} )
+{
+  for( var i = 1; i <= range; i++ ){
+    print('$number X $i = ${number * i}');
+  }
+}
+
+// Boss
 void main() {
+  table(range: 9, number: 8);
 
-  Map<String, int> studentAge = {
-    "ali": 19,
-    "bilal": 20,
-    "hina": 18,
+  // sum(5, 6);
+  // square(7);
+  // sum(2, 3);
+  // sum(59, 1);
 
-  };
+  // displayMyString("#"); // actual arguments
+  // displayMyString("+");
+  // displayStars();
+}
 
-  studentAge['gia'] = 23;
-  studentAge['bilal'] = 21;
-
-  print(studentAge);
-
-  Map<String, dynamic> studentInfo = {
-    'rollNo': 1,
-    "name": "rubab",
-    'uni': "Bagh Uni",
-    "married": false,
-    "gpa": 2.1,
-  };
-
-  print(studentInfo);
+void displayStars()
+{
+  print('**************************');
 }
