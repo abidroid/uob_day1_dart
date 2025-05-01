616b26ffe78a09453dae40c5f680d7c098781b7e
Abid <abidroid1@gmail.com>
2025-05-01 09:47:41 +0500

    has a relationship

--- bin/uob_day1_dart.dart
+++ bin/uob_day1_dart.dart
@@ -16,6 +16,7 @@ class Doctor extends Person {
   late String spe;
   late int fee;
   late String clinicTime;
+  late Address address; // has a relationship
 
   Doctor({
     required String name,
@@ -24,8 +25,8 @@ class Doctor extends Person {
     required this.spe,
     required this.fee,
     required this.clinicTime,
+    required this.address,
   }) : super(name: name, gender: gender, dob: dob);
-
 
   @override
   void display() {
@@ -61,9 +62,11 @@ void main() {
     spe: 'Cardiologist',
     fee: 2500,
     clinicTime: '9am - 2pm',
+    address: Address(houseNum: '1', streetNum: '2', city: 'Bagh'),
   );
 
   azam.display();
+  print(azam.address.city);
 
   Doctor hina = Doctor(
     name: 'Hina',
@@ -72,9 +75,11 @@ void main() {
     spe: 'ENT',
     fee: 2500,
     clinicTime: '2pm - 6pm',
+    address: Address(houseNum: '3', streetNum: '8', city: 'PSH'),
   );
   print("*******************************");
   //hina.display();
+  print(hina.address.city);
 
   Beggar beggar1 = Beggar(
     name: 'Bilal',
@@ -84,12 +89,23 @@ void main() {
   );
   print("*******************************");
 
- // beggar1.display();
+  // beggar1.display();
   //beggar1.displayBeggar();
 
-  List<Person> doctors = [
-    azam,
-    hina,
-    beggar1,
-  ];
+  List<Person> doctors = [azam, hina, beggar1];
+
+}
+
+class Address {
+  late String houseNum;
+  late String streetNum;
+  late String city;
+  String? country;
+
+  Address({
+    required this.houseNum,
+    required this.streetNum,
+    required this.city,
+    this.country,
+  });
 }
