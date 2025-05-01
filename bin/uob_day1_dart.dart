dcf9f9deae193644aa5e493e9bb7263b2219af70
Abid <abidroid1@gmail.com>
2025-05-01 09:37:15 +0500

    inheritance + method overrding

--- bin/uob_day1_dart.dart
+++ bin/uob_day1_dart.dart
@@ -1,40 +1,95 @@
-// User defined DataType
-class Student {
+class Person {
   late String name;
-  late String fatherName;
-  String? mobileNumber;
+  late String gender;
+  late String dob;
 
-  // default constructor
-  // parameterized constructor
-  Student({required this.name,required this.fatherName, this.mobileNumber});
+  Person({required this.name, required this.gender, required this.dob});
 
   void display() {
-    print('Name: $name');
-    print("Father Name: $fatherName");
-    print('Mobile # : $mobileNumber');
+    print("Name:   $name");
+    print('Gender: $gender');
+    print('DOB:    $dob');
+  }
+}
+
+class Doctor extends Person {
+  late String spe;
+  late int fee;
+  late String clinicTime;
+
+  Doctor({
+    required String name,
+    required String gender,
+    required String dob,
+    required this.spe,
+    required this.fee,
+    required this.clinicTime,
+  }) : super(name: name, gender: gender, dob: dob);
+
+
+  @override
+  void display() {
+    super.display();
+    print("SPE:  $spe");
+    print("FEE:  $fee");
+    print('Time: $clinicTime');
+  }
+}
+
+class Beggar extends Person {
+  late int dailyIncome;
+
+  Beggar({
+    required String name,
+    required String gender,
+    required String dob,
+    required this.dailyIncome,
+  }) : super(name: name, gender: gender, dob: dob);
+
+  @override
+  void display() {
+    super.display();
+    print('Income: $dailyIncome');
   }
 }
 
 void main() {
-  Student hina = Student(name: 'Hina', fatherName: 'Khan', mobileNumber: '0838838383');
+  Doctor azam = Doctor(
+    name: 'Azam Jan',
+    gender: 'M',
+    dob: '1st Apr',
+    spe: 'Cardiologist',
+    fee: 2500,
+    clinicTime: '9am - 2pm',
+  );
 
-  Student bilal = Student(name: 'Bilal', fatherName: 'Ali');
+  azam.display();
 
-  List<Student> studentList = [
+  Doctor hina = Doctor(
+    name: 'Hina',
+    gender: 'F',
+    dob: '1st May',
+    spe: 'ENT',
+    fee: 2500,
+    clinicTime: '2pm - 6pm',
+  );
+  print("*******************************");
+  //hina.display();
+
+  Beggar beggar1 = Beggar(
+    name: 'Bilal',
+    gender: 'M',
+    dob: '2 Apr',
+    dailyIncome: 10000,
+  );
+  print("*******************************");
+
+ // beggar1.display();
+  //beggar1.displayBeggar();
+
+  List<Person> doctors = [
+    azam,
     hina,
-    bilal,
-    Student(name: 'Rubeena', fatherName: 'Riaz'),
+    beggar1,
   ];
-
-  for( Student s in studentList){
-    s.display();
-    print('*******************');
-  }
-}
-
-class Doctor {
-  late String name;
-  late String spe;
-  late int fee;
-  late String clinicAddress;
 }
